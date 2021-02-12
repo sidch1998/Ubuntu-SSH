6c08b4e96269c971981f2062b732c1e86d09c3b8
Angga R <angga@linuxmail.org>
2021-02-12 07:55:17 +0700

    add git config

--- ubuntu-run.sh
+++ ubuntu-run.sh
@@ -1,1 +1,4 @@
-echo this can run commands in terminal tho && q
+git config --global credential.helper store
+git config --global user.name Angga
+git config --global user.email angga@linuxmail.org
+alias p='git cherry-pick'
